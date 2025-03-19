a4852d30bc16260acf459c26349b22ad77680e49
ashutosh229 <akumarjha875@gmail.com>
2025-03-20 02:11:32 +0530

    added the links to github repos

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -66,8 +66,11 @@ const RecentProjects = () => {
                 </div>
 
                 <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
+                  <p
+                    className="flex lg:text-xl md:text-xs text-sm text-purple"
+                    onClick={() => window.open(item.link, "_blank")}
+                  >
+                    Check Github Repo
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
