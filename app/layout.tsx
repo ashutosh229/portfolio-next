542373dfac8bde81f43756c1eef726316f3cb0ed
ashutosh229 <akumarjha875@gmail.com>
2026-01-01 04:28:44 +0530

    feat: update the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,26 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { ClientProviders } from "./provider";
 
 import "./globals.css";
-import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
+export const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ashutosh's Portfolio",
-  description: "Next.js portfolio of Ashutosh Kumar Jha",
+  description:
+    "Full Stack Developer & AI Enthusiast - Portfolio showcasing projects, skills, and experience",
+  openGraph: {
+    title: "Ashutosh's Portfolio",
+    description: "Full Stack Developer & AI Enthusiast",
+    images: ["https://lovable.dev/opengraph-image-p98pqg.png"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@ashutoshkj0390",
+    images: ["https://lovable.dev/opengraph-image-p98pqg.png"],
+  },
   icons: {
     icon: "/favicons/logo.jpg",
   },
@@ -23,16 +35,13 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.jpeg" sizes="any" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.16.0/devicon.min.css"
+        ></link>
       </head>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <ClientProviders>{children}</ClientProviders>
       </body>
     </html>
   );
